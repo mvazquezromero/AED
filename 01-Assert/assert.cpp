d5d51640bae40c85fd6e449402884106c8d18f3b
Matias Vazquez Romero <82189978+mvazquezromero@users.noreply.github.com>
2021-05-08 00:33:48 -0300

    Update assert.cpp

--- 01-Assert/assert.cpp
+++ 01-Assert/assert.cpp
@@ -1,15 +1,15 @@
 #include <iostream>
-#define NDEBUG
+//#define NDEBUG
 #include <cassert>
 
 int main(){
     assert(2 + 2 == 4); // int + int ok
 
-    assert("a" +"a" == "aa"); // char + char = string ok 
+    assert('a' != "a"); // char != string ok 
     
-    assert(3.14159265359 == 3.14159265359); // float == float
+    assert(3.14 == 3.14); // float == float
 
-    assert(1 != -1); // signo
+    assert(1 != -1); // unsigned
 
     assert("Hola mundo" != "hola mundo"); // string
 
